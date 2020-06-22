625f609ab28b8c0c770d0ed2b00c3d95d3884ff7
MKrsna <krsna@localhost.localdomain>
2020-06-22 11:24:39 +0530

    Search added

--- src/modules/products/containers/products/products.component.ts
+++ src/modules/products/containers/products/products.component.ts
@@ -1,11 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit, OnDestroy } from '@angular/core';
-// import { ProductService } from '@modules/products/services';
-// import { Product } from '@modules/products/models';
-// import { ProductService } from '@common/services/products.service';
 import { CartService } from '@common/services/cart.service';
 import { Product, ParentProducts } from '@common/models';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'sb-products',
@@ -16,43 +14,50 @@ export class ProductsComponent implements OnInit, OnDestroy {
     parentProductId;
     showAllProducts: boolean = false;
     products: Product[];
+
+    isSearching:boolean=false;
+
     sub: Subscription;
     productTypesChild: ParentProducts[] = [];
     constructor(private cartService: CartService, private route: ActivatedRoute) {
         this.sub = this.route.params.subscribe(params => { this.parentProductId = params['id']; this.refreshContent() });
-        // this.parentProductId = this.route.snapshot.paramMap.get('id');
         this.showAllProducts = this.parentProductId ? false : true;
     }
+
+    searchForm = new FormGroup({
+        search: new FormControl('', Validators.required),
+    });
+
     ngOnDestroy() {
         this.sub.unsubscribe();
     }
 
     ngOnInit() { }
 
+    search() {
+        this.isSearching = true;
+        let searchTerm: string = this.searchForm.get("search").value.toLowerCase();
+        this.products = this.products.filter(v => v.title.toLowerCase().indexOf(searchTerm) > -1 || v.details.toLowerCase().indexOf(searchTerm) > -1);
+    }
+
     refreshContent() {
+        this.isSearching = false;
         this.productTypesChild = [];
         if (this.parentProductId) {
             this.products = this.cartService.getProductsUnderParent(this.parentProductId);
-            // console.log("ss",)
             this.cartService.getProductTypesUnderParent(this.parentProductId).forEach(f => {
                 let val = this.cartService.getChildProductTypeTitle(f);
-                // console.log("asasas",val);
                 this.productTypesChild.push(val);
-
             });
-
             console.log("aa", this.products, this.productTypesChild);
-
         }
         else {
-            // alert("all")
             this.products = this.cartService.getProducts();
         }
     }
 
     addToCart(product: Product) {
         this.cartService.addToCart(product);
-        // console.log('added to cart');
         this.refreshContent();
     }
 }
