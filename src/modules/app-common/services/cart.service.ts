94d492380c2855e44ff83971513e4a5b6063a7c3
MKrsna <krsna@localhost.localdomain>
2020-07-19 17:29:22 +0530

    mail sorted

--- src/modules/app-common/services/cart.service.ts
+++ src/modules/app-common/services/cart.service.ts
@@ -105,11 +105,12 @@ export class CartService {
 
 
     sendOrder(name, email, specs): Observable<Object> {
-        return this.http.post("http://127.0.0.1:5000/mail", {
+        let data = {
             "name": name, "email": email, "specs": specs, "order": this.getCart().cartItem.map(v => {
-                return "Item ID: KE" + (v.product.details + '000' + v.product.id).slice(-3) + ", Qty:" + v.qty
+                return "Item ID: KE" + ('000' + v.product.id).slice(-3) + ", Product Detail: " + v.product.details +", Qty:" + v.qty
             })
-        }, { responseType: 'text' })
+        };
+        return this.http.post("http://13.233.104.142:5000/",data , { responseType: 'text' })
 
     }
 }
