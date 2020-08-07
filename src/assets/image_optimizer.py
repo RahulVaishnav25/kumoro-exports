d45cbeeb2ef1f86dea9382e1963a8cd6ce8668be
Monika Rai <rai20monika@gmail.com>
2020-08-07 11:16:17 +0530

    finalizing

--- src/assets/image_optimizer.py
+++ src/assets/image_optimizer.py
@@ -14,5 +14,7 @@ for subdir, dirs, files in os.walk(r'images'):
             with open(filepath, 'r+b') as f:
                 with Image.open(f) as image:
                     if  image.size[0]>300 and image.size[1]>300 :
-                        cover = resizeimage.resize_cover(image, [300, 300])
-                        cover.save(filepath.replace("images/","images_opt/"), image.format)
+                        cover = resizeimage.resize_contain(image, [300, 300])
+                        # print(image.format)
+                        # cover.save(filepath.replace("images\\","images_opt\\").replace(".jpg",".png") , image.format)
+                        cover.save(filepath.replace("images\\","images_opt\\") , "PNG")
